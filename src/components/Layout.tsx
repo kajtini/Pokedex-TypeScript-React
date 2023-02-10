0d02aaa6b944a96c5a7d699f6f9271a1af9e2a8d
Kajetan Kowalski <kowalskikajetan824@gmail.com>
2023-02-10 13:24:40 +0100

    small loading change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,9 @@ type LayoutProps = {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="max-w-[1200px] w-full px-8 relative mb-8">{children}</div>
+    <div className="max-w-[1200px] w-full px-8 relative mb-8 min-h-screen">
+      {children}
+    </div>
   );
 }
 
